1c80f72c11044b6eba96d765d8623d9ba808662b
viettien <viettienn97@gmail.com>
2018-01-19 16:25:33 +0700

    request

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
     @movie_title = ScrapingImdb.new.title
+    @posts_pending = PostReview.pending.newest.paginate page: params[:page],
+     per_page: Settings.paginate_number.per_page
   end
 
   def about
